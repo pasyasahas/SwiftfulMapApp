ccb332c92707e3917ff8d1c7cae2ce0ad3041ad1
Sahas Pasya <pasyasahas17@gmail.com>
2024-10-02 11:45:44 -0500

    [Feature] Added architecture, Add DataService

---
+++ SwiftfulMapApp/Models/Locations.swift
@@ -0,0 +1,18 @@
+//
+//  Locations.swift
+//  SwiftfulMapApp
+//
+//  Created by Pasya Sahas on 10/2/24.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
