8d63a9d2152a6c20a3bb7c0affd9cbf5c2677abb
Sahas Pasya <pasyasahas17@gmail.com>
2024-10-03 12:55:04 -0500

    [Feature] Add Next Button Functionality

--- SwiftfulMapApp/Views/LocationPreviewView.swift
+++ SwiftfulMapApp/Views/LocationPreviewView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct LocationPreviewView: View {
     
+    @EnvironmentObject private var vm: LocationsViewModel
+    
     let location: Location
     
     var body: some View {
@@ -39,6 +41,7 @@ struct LocationPreviewView: View {
         LocationPreviewView(location: LocationsDataService.locations.first!)
             .padding()
     }
+    .environmentObject(LocationsViewModel())
 }
 
 extension LocationPreviewView {
@@ -82,7 +85,7 @@ extension LocationPreviewView {
     
     private var nextButton: some View{
         Button {
-            
+            vm.nextButtonPressed()
         } label: {
             Text("Next")
                 .font(.headline)
